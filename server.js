4d008101b450b513a6b0524ee2e298de5d6a872a
jason9294 <63494593+jason9294@users.noreply.github.com>
2022-04-19 19:07:51 +0800

    .

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const cors = require('cors')
+const axios = require('axios')
+require('dotenv').config()
+const app = express()
+const port = 5000
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static('dist'));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/sendMsgToChannel', async(req, res) => {
+  console.log(req.body.msg)
+    await axios({
+        method:"post",
+        url:`https://discord.com/api/channels/${req.body.channelID}/messages`,
+        headers:{
+            Authorization: `Bot ${process.env.BOT_TOKEN}`
+        },
+        data:{
+            content:req.body.msg
+        }
+    }) 
+
+    res.send("Done!")
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <input type="text" placeholder="頻道ID" v-model="channel">
+  <input type="text" placeholder="請輸入訊息" v-model="message">
+  <button v-on:click="sendMsg">發送訊息</button>
+</template>
+
+
+<script setup>
+import { ref } from "@vue/reactivity"
+import axios from 'axios'
+
+
+const message = ref("")
+const channel = ref("")
+
+const sendMsg = async() => {
+  alert(message.value)
+  const res = await axios({
+    method: "post",
+    url: "http://localhost:5000/sendMsgToChannel",
+    data: {
+      msg: message.value,
+      channelID: channel.value
+    }
+  })
+
+  alert(res.data)
+}
+</script>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
